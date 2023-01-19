6aff5fa14c4117f6c64bbdc695efd42780c38aaf
oio337a <oio337a@gmail.com>
2023-01-19 19:55:20 +0900

    feat: add view components

---
+++ src/view/View.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Canvas = styled.div``;
+const Node = styled.div``;
+
+const View = () => {
+  return (
+    <Canvas>
+      <Node>루트</Node>
+    </Canvas>
+  );
+};
+
+export default View;
