04eede297a6bcd763b408a41cf67e7a7136b2f16
soong-e <yujelee@student.42seoul.kr>
2023-01-27 21:53:34 +0900

    feat: separate Directory, Command Component
    separate Directory, Command Component using Conditional operator
    delete direc state, using root instead
    make root state to save root Tree for "~" path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { useState, useEffect } from 'react';
+import Header from './header/Header';
+import Command from './command/Command';
+import View from './view/View';
+import Footer from './infoBar/Footer';
+import Directory from './init/Directory';
+import Tree from './model/Tree';
+import { TYPE } from './constants/Type';
+
 function App() {
-  return <div className='App'></div>;
+  const [record, setRecord] = useState([]); //command 기록
+  const [pwd, setPwd] = useState([]); // 현재 경로(스택)
+  const [root, setRoot] = useState(); // Root
+
+  const onDirec = (direc) => {
+    let root = new Tree(direc, TYPE.DIR);
+    setPwd([root]);
+    setRoot(root);
+  };
+
+  return (
+    <div>
+      <Header />
+      {pwd.length > 0 ? (
+        <div className='App'>
+          <Command
+            record={record}
+            setRecord={setRecord}
+            pwd={pwd}
+            setPwd={setPwd}
+            root={root}
+          />
+          <View directory={root.getName()} />
+        </div>
+      ) : (
+        <Directory onDirec={onDirec} />
+      )}
+      <Footer />
+    </div>
+  );
 }
+
+/* 명령어
+cp mv mkdir rm touch chmod cd pwd
+directed graph, vertex: 
+*/
 
 export default App;
