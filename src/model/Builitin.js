f5bc20cdf84f98fdfc10bb24f21ced3374f97f57
soong-e <yujelee@student.42seoul.kr>
2023-02-01 21:49:20 +0900

    feat: implement Builtin function
    implement mv, rm
    modify cp, touch

--- src/model/Builitin.js
+++ src/model/Builitin.js
@@ -1,13 +1,12 @@
-import { useMemo } from 'react';
 import { TYPE } from '../constants/Type';
 import Tree from './Tree';
 
 const Builtin = {
   getNode(cwd, splittedPath) {
-    const tempWd = cwd;
+    const tempWd = [...cwd];
 
     for (let i = 0; i < splittedPath.length; ++i) {
-      if (splittedPath[i] === '..') {
+      if (splittedPath[i] === '..' && cwd.length != 1) {
         tempWd.pop();
       } else if (splittedPath[i] !== '.') {
         const subTree = tempWd.at(-1).getChild().get(splittedPath[i]);
@@ -18,13 +17,12 @@ const Builtin = {
             return undefined;
           }
         }
-        if (subTree.getType() === TYPE.DIR) {
-          tempWd.push(subTree);
-        }
         if (i === splittedPath.length - 1) {
           return { leafNode: subTree, lastDir: tempWd.at(-1) };
         }
-        if (subTree.getType() === TYPE.FILE) {
+        if (subTree.getType() === TYPE.DIR) {
+          tempWd.push(subTree);
+        } else if (subTree.getType() === TYPE.FILE) {
           console.log('not a directory');
         }
       }
@@ -36,7 +34,7 @@ const Builtin = {
     const path = arg[0].split('/');
 
     path.forEach((element) => {
-      if (element === '..') {
+      if (element === '..' && cwd.length != 1) {
         cwd.pop();
       } else if (element !== '.') {
         const subTree = cwd.at(-1).getChild().get(element);
@@ -51,11 +49,13 @@ const Builtin = {
     });
   },
 
-  chmod(arg, props) {
-    //보류
-  },
-
   cp(arg, { cwd }) {
+    if (arg.length !== 2) {
+      //에러 핸들링
+      console.log(
+        'usage: cp [-R [-H | -L | -P]] [-fi | -n] [-aclpsvXx] source_file target_file'
+      );
+    }
     const splittedSrc = arg[0].split('/');
     const src = Builtin.getNode(cwd, splittedSrc);
     if (src === undefined || src.leafNode === undefined) {
@@ -65,7 +65,6 @@ const Builtin = {
       console.log('//arg[0] is a directory (not copied).');
       return;
     }
-    //src.leatNode.type = TYPE.FILE
 
     const splittedDest = arg[1].split('/');
     const dest = Builtin.getNode(cwd, splittedDest);
@@ -73,10 +72,17 @@ const Builtin = {
       console.log('//error cp: no such file or directory:arg[0]');
       return;
     }
-    // console.log(dest.leafNode.getName(), dest.lastDir.getName());
-    if (dest.leafNode === undefined || dest.leafNode.getType() === TYPE.DIR) {
-      dest.lastDir.addChild(src.leafNode);
+    if (dest.leafNode === undefined) {
+      dest.lastDir.addChild(
+        new Tree(splittedDest.at(-1), src.leafNode.getType())
+      );
+    } else if (dest.leafNode.getType() === TYPE.DIR) {
+      dest.leafNode.addChild(src.leafNode);
     } else if (dest.leafNode.getType() === TYPE.FILE) {
+      if (src.leafNode.getName() === dest.leafNode.getName()) {
+        console.log('cp: a and a are identical (not copied).');
+        return;
+      }
       dest.lastDir.addChild(src.leafNode, true);
     }
   },
@@ -88,54 +94,74 @@ const Builtin = {
     });
   },
 
-  mv(arg, props) {
-    /*
-      이름 바꾸는 거
-
-      mv aa bb => aa 를 bb 로 변경 (같은 레벨에 bb란 파일/디렉 이 없을때)
-      cc가 dir이면 안으로 들어가고 file이면 cc 가 디렉이 아닙니다.
-      mv aa bb cc => aa bb -> cc 안으로 들어간다. 
-    */
-    /*
-      옮기기
-
-      1. mv aa bb => 같은 레벨에 bb 란 디렉이 존재하면 이동
-      2. mv cc ./cc/bb  cc가 이동할때 자신을 지나면 에러
-              -> mv: rename cc to ./cc/bb/cc: Invalid argument
-    */
-    // 파일이나 폴더 이동
-    // 현재 경로의 파일이나 폴더 삭제
+  mv(arg, { cwd }) {
+    if (arg.length === 1) {
+      console.log('mv는 기본적으로 인자가 두개란다??zzzzz');
+      return;
+    }
+    const splittedLastArg = arg.at(-1).split('/');
+    const lastArg = Builtin.getNode(cwd, splittedLastArg);
+    if (arg.length > 2) {
+      if (lastArg === undefined) {
+        console.log('is not a directory');
+        return;
+      } else if (
+        lastArg.leafNode === undefined ||
+        lastArg.leafNode.getType() !== TYPE.DIR
+      ) {
+        console.log('is not a directory');
+        return;
+      }
+    }
+    const dummy = [];
+    const errorDummy = [];
+    let tempTree;
+    for (let i = 0; i < arg.length - 1; ++i) {
+      const splittedArg = arg[i].split('/');
+      tempTree = Builtin.getNode(cwd, splittedArg);
+      if (tempTree === undefined || tempTree.leafNode === undefined) {
+        errorDummy.push([arg[i], 'No such file or directory']);
+      } else {
+        dummy.push(tempTree);
+      }
+    }
+    if (
+      arg.length === 2 &&
+      dummy.length === 1 &&
+      (lastArg.leafNode === undefined ||
+        lastArg.leafNode.getType() === TYPE.FILE)
+    ) {
+      tempTree = dummy[0].leafNode;
+      dummy[0].lastDir.getChild().delete(tempTree.getName());
+      lastArg.lastDir.addChild(
+        new Tree(splittedLastArg.at(-1), TYPE.FILE),
+        true
+      );
+    } else {
+      dummy.forEach((element) => {
+        tempTree = element.leafNode;
+        element.lastDir.getChild().delete(element.leafNode.getName());
+        lastArg.leafNode.addChild(tempTree);
+      });
+    }
+    errorDummy.forEach((err) => {
+      console.log('error', err[0], err[1]);
+    });
   },
 
   rm(arg, { cwd, root }) {
-    //현재 경로의 파일이나 폴더 삭제
-    // 디렉토리 인경우 하위 데이터 도 삭제할지 or
-    // 디렉토리인 경우 안된다고 반환할지
-    // -rf 옵션 적용하기
-    // r = 하위에 있는 것도 삭제
-    // f = 묻지 않고 삭제
-
-    // rm -rf file
-    // rm -rf ../direc
-    // 파일인지 디렉인지 확인 언제 할꺼임???
     for (let i = 0; i < arg.length; i++) {
-      const tempArg = arg[i].split('/');
-      if (arg[0] !== '-rf') {
-        console.log(`${arg[i]}을 삭제 하시겠습니까?? Yes / No`);
-        const type = Builtin.getNode(cwd, tempArg);
-        if (type !== undefined) {
-          type = type.lastDir.get(tempArg.at(-1)).getType();
-        }
-        if (type === TYPE.DIR) {
-          console.log(`${arg[i]} is a directory`);
-          return;
-        }
+      if (arg[i] === '-rf') continue;
+      const splittedArg = arg[i].split('/');
+      const node = Builtin.getNode(cwd, splittedArg);
+      console.log(`${arg[i]}을 삭제 하시겠습니까?? Yes / No`);
+      if (node === undefined || node.leafNode === undefined) return;
+      const type = node.leafNode.getType();
+      if (i === 0 && type === TYPE.DIR) {
+        console.log(`${arg[i]} is a directory`);
+        return;
       }
-      if (arg[0] === '-rf') continue;
-      if (tempArg.length !== 1) {
-        Builtin.getNode(cwd, tempArg).leafNode.delete(tempArg.at(-1));
-      }
-      cwd.at(-1).getChild().delete(tempArg.at(-1));
+      node.lastDir.getChild().delete(splittedArg.at(-1));
     }
   },
 
@@ -143,10 +169,9 @@ const Builtin = {
     arg.forEach((element) => {
       const splittedPath = element.split('/');
       const path = Builtin.getNode(cwd, splittedPath);
-      if (path !== undefined) {
-        const file = new Tree(splittedPath.at(-1), TYPE.FILE);
-        path.lastDir.addChild(file);
-      }
+      if (path === undefined || path.leafNode !== undefined) return;
+      const file = new Tree(splittedPath.at(-1), TYPE.FILE);
+      path.lastDir.addChild(file);
     });
   },
 };
