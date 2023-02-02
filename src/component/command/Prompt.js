38df7d8288dad715c308e95dfe08a3cc90e84690
soong-e <yujelee@student.42seoul.kr>
2023-02-02 21:59:58 +0900

    feat: add history hooking arrow key

--- src/component/command/Prompt.js
+++ src/component/command/Prompt.js
@@ -1,16 +1,34 @@
-import styled from 'styled-components';
 import { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
 
-const Prompt = ({ addCommand }) => {
+const Prompt = ({ cmd, addCommand }) => {
   const focusInput = useRef(null);
+  const historyIndex = useRef(cmd.length + 1);
   const [input, setInput] = useState('');
 
   useEffect(() => {
     focusInput.current.focus();
   }, []);
 
+  const changeIndex = (n) => {
+    const tempIndex = historyIndex.current + n;
+    if (0 <= tempIndex && tempIndex <= cmd.length) {
+      historyIndex.current = tempIndex;
+    }
+  };
+
   const handleOnKeyPress = (e) => {
+    if (e.key === 'ArrowUp') {
+      changeIndex(-1);
+      setInput(cmd[historyIndex.current].join(' '));
+    }
+    if (e.key === 'ArrowDown') {
+      changeIndex(1);
+      if (historyIndex.current === cmd.length) setInput('');
+      else setInput(cmd[historyIndex.current].join(' '));
+    }
     if (e.key === 'Enter') {
+      historyIndex.current = cmd.length + 1;
       addCommand(input);
       setInput('');
     }
