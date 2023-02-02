9be91e09e203698c6352f614ba19bbba71588ae9
soong-e <yujelee@student.42seoul.kr>
2023-02-02 21:59:05 +0900

    feat: generate view

--- src/component/command/History.js
+++ src/component/command/History.js
@@ -1,10 +1,28 @@
+// import { useMemo } from 'react';
 import styled from 'styled-components';
+
+const CommandLine = ({ cmd }) => {
+  return <CommandBox>{cmd.join(' ')}</CommandBox>;
+};
+
+const ErrorLine = ({ error }) => {
+  return (
+    <div>
+      {error.map((x, idx) => (
+        <ErrorBox key={idx}>{x}</ErrorBox>
+      ))}
+    </div>
+  );
+};
 
 const History = ({ record }) => {
   return (
     <Box>
-      {record.map((x, idx) => (
-        <div key={idx}>{x.join(' ')}</div>
+      {record.cmd.map((x, idx) => (
+        <Container key={idx}>
+          <CommandLine cmd={x} />
+          <ErrorLine error={record.error[idx]} />
+        </Container>
       ))}
     </Box>
   );
@@ -15,3 +33,7 @@ export default History;
 const Box = styled.div`
   height: 400px;
 `;
+
+const Container = styled.div``;
+const CommandBox = styled.div``;
+const ErrorBox = styled.div``;
