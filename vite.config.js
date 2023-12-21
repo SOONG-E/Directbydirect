34f5846bdc53615ddcb9d20bd1e4ffd3c048b18f
soong-e <yujelee@student.42seoul.kr>
2023-12-21 16:09:50 +0900

    chore: 절대경로 설정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
+    alias: {
+      src: '/src',
+      component: '/src/component',
+    },
   },
 });
