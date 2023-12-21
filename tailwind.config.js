9dc7d4283a662ac791929b3b901cb872367b8eed
soong-e <yujelee@student.42seoul.kr>
2023-12-21 18:48:12 +0900

    style: 기본 검정색 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      midblack: '#353535',
+    },
   },
   plugins: [],
-}
-
+};
