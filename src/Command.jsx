3a7349ccda14a68357cfb2185b159fe779957997
oio337a <oio337a@gmail.com>
2023-12-29 19:59:56 +0900

    [feat]: Command components

---
+++ src/Command.jsx
@@ -0,0 +1,8 @@
+export default function Command() {
+  return (
+    <div className='mx-2 flex justify-between'>
+      <div className='text-xl font-bold text-green-400'>&gt;</div>
+      <input className='bg-black text-white' type='text' />
+    </div>
+  );
+}
