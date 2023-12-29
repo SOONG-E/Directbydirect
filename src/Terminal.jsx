dd20c2a923f8b6f3bbd547f2fd805ffc00a0e993
oio337a <oio337a@gmail.com>
2023-12-29 20:28:06 +0900

    feat: add history props

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -1,13 +1,16 @@
 import History from 'src/History';
 import TopBar from 'src/TopBar';
-import Command from 'src/command';
+import Prompt from 'src/Prompt';
+import { useState } from 'react';
 
 export default function Terminal() {
+  const [history, setHistory] = useState([]);
+
   return (
     <div className='flex h-5/6 w-1/3 flex-col justify-between rounded-md bg-black shadow-lg'>
       <TopBar />
-      <History />
-      <Command />
+      <History history={history} />
+      <Prompt setHistory={setHistory} />
     </div>
   );
 }
