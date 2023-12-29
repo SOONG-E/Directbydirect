d8a486e1e27fbca911e93cc843dbca062d309fca
oio337a <oio337a@gmail.com>
2023-12-29 20:10:18 +0900

    feat : enter event

--- src/Command.jsx
+++ src/Command.jsx
@@ -1,8 +1,13 @@
 export default function Command() {
+  const onEnter = (e) => {
+    if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
+      console.log('Enter');
+    }
+  };
   return (
     <div className='mx-2 flex justify-between'>
       <div className='text-xl font-bold text-green-400'>&gt;</div>
-      <input className='bg-black text-white' type='text' />
+      <input onKeyDown={onEnter} className='bg-black text-white' type='text' />
     </div>
   );
 }
