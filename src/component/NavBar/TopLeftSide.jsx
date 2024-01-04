55096a94494334870e1287ddf9c28a6f842576c2
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-04 16:16:32 +0900

    style: prettier 적용

--- src/component/NavBar/TopLeftSide.jsx
+++ src/component/NavBar/TopLeftSide.jsx
@@ -1,7 +1,7 @@
 const TopLeftSide = () => {
   return (
-    <div className='flex text-midblack font-bold text-sm items-center'>
-      <img src='MiniLogo.png' className='w-6 mr-4' />
+    <div className='flex items-center text-sm font-bold text-midblack'>
+      <img src='MiniLogo.png' className='mr-4 w-6' />
       <div>DirectByDirect</div>
       <div className='ml-4'>Gits</div>
     </div>
