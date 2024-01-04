fed2ab819cd68d091d3ba4bdef67eb7797b3108e
soong-e <yujelee@student.42seoul.kr>
2024-01-04 09:20:31 +0900

    style: 오른쪽 엘리멘트들 스타일 수정

--- src/component/NavBar/TopRightSide.jsx
+++ src/component/NavBar/TopRightSide.jsx
@@ -1,11 +1,11 @@
 const TopRightSide = () => {
   return (
-    <div className='grid grid-cols-5 gap-4 place-items-center'>
-      <img src='Battery.png'></img>
-      <img src='Find.png'></img>
-      <img src='Switch.png'></img>
-      <img src='Wifi.png'></img>
-      <img src='Siri.png'></img>
+    <div className='grid grid-cols-5 gap-7 place-items-center pr-5'>
+      <img src='Battery.png' className='w-5'></img>
+      <img src='Find.png' className='w-5'></img>
+      <img src='Switch.png' className='w-5'></img>
+      <img src='Wifi.png' className='w-5'></img>
+      <img src='Siri.png' className='w-5'></img>
     </div>
   );
 };
