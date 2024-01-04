4433e3de77643fa06554225cfaec5c049d124560
soong-e <yujelee@student.42seoul.kr>
2024-01-04 13:55:47 +0900

    refactor: 포매터 적용

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -3,13 +3,12 @@ import TopRightSide from 'src/component/NavBar/TopRightSide';
 
 const NavBar = () => {
   return (
-    <div className='relative w-full h-[32px] backdrop-blur-md shadow-lg'>
-      <div className='w-full h-[32px] opacity-20 bg-[#d2d2d2] drop-shadow'>
+    <div className='relative h-[32px] w-full shadow-lg backdrop-blur-md'>
+      <div className='h-[32px] w-full bg-[#d2d2d2] opacity-20 drop-shadow'></div>
+      <div className='absolute top-0 flex h-full w-full justify-between px-5'>
+        <TopLeftSide />
+        <TopRightSide />
       </div>
-        <div className='absolute top-0 flex w-full h-full justify-between px-5'>
-          <TopLeftSide />
-          <TopRightSide />
-        </div>
     </div>
   );
 };
--- src/component/NavBar/TopRightSide.jsx
+++ src/component/NavBar/TopRightSide.jsx
@@ -1,6 +1,6 @@
 const TopRightSide = () => {
   return (
-    <div className='grid grid-cols-5 gap-7 place-items-center'>
+    <div className='grid grid-cols-5 place-items-center gap-7'>
       <img src='Battery.png' className='w-5'></img>
       <img src='Find.png' className='w-5'></img>
       <img src='Switch.png' className='w-5'></img>
