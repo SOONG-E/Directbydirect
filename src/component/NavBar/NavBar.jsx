dfefedaf17fb2f8e1869cf6be0dec9ae69c87e61
soong-e <yujelee@student.42seoul.kr>
2024-01-04 11:19:24 +0900

    style: 상단바 그림자, 색 변경

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -3,8 +3,8 @@ import TopRightSide from 'src/component/NavBar/TopRightSide';
 
 const NavBar = () => {
   return (
-    <div className='relative w-full h-[32px] backdrop-blur-md'>
-      <div className='w-full h-[32px] opacity-30 bg-neutral-300 drop-shadow'>
+    <div className='relative w-full h-[32px] backdrop-blur-md shadow-lg'>
+      <div className='w-full h-[32px] opacity-20 bg-[#d2d2d2] drop-shadow'>
       </div>
         <div className='absolute top-0 flex w-full h-full justify-between px-5'>
           <TopLeftSide />
