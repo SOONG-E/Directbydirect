bd4c3bab71418721ecae288067659a6a8bf995bc
soong-e <yujelee@student.42seoul.kr>
2024-01-04 09:30:44 +0900

    fix: 글자 투명도 낮아진 거 수정

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -3,13 +3,13 @@ import TopRightSide from 'src/component/NavBar/TopRightSide';
 
 const NavBar = () => {
   return (
-    <div className='w-full h-[32px] backdrop-blur-md'>
+    <div className='relative w-full h-[32px] backdrop-blur-md'>
       <div className='w-full h-[32px] opacity-30 bg-neutral-300 drop-shadow'>
-        <div className='flex h-full justify-between mx-5'>
+      </div>
+        <div className='absolute top-0 flex w-full h-full justify-between px-5'>
           <TopLeftSide />
           <TopRightSide />
         </div>
-      </div>
     </div>
   );
 };
