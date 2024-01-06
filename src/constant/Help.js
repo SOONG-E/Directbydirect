2bd389642ca026878643d6afb0f6d8a286ecb5cb
oio337a <oio337a@gmail.com>
2024-01-06 17:37:50 +0900

    feat: add help text

---
+++ src/constant/Help.js
@@ -0,0 +1,50 @@
+export const HELP = [
+  {
+    CMD: 'cd',
+    TEXT: 'Change directory',
+  },
+  {
+    CMD: 'clear',
+    TEXT: 'Clear terminal',
+  },
+  {
+    CMD: 'cp',
+    TEXT: 'Copy file',
+  },
+  {
+    CMD: 'help',
+    TEXT: 'List all commands',
+  },
+  {
+    CMD: 'ls',
+    TEXT: 'List directory',
+  },
+  {
+    CMD: 'mkdir',
+    TEXT: 'Create directory',
+  },
+  {
+    CMD: 'mv',
+    TEXT: 'Move file',
+  },
+  {
+    CMD: 'pwd',
+    TEXT: 'Print working directory',
+  },
+  {
+    CMD: 'rm',
+    TEXT: 'Remove file',
+  },
+  {
+    CMD: 'rmdir',
+    TEXT: 'Remove directory',
+  },
+  {
+    CMD: 'rm',
+    TEXT: 'Remove file',
+  },
+  {
+    CMD: 'touch',
+    TEXT: 'Create file',
+  },
+];
