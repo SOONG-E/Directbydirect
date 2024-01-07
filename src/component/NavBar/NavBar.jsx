a83ef597290dc050e6206fcf011a49afa637e60d
soong-e <yujelee@student.42seoul.kr>
2024-01-08 00:55:43 +0900

    refactor: 필요없는 요소 수정

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -3,11 +3,12 @@ import TopRightSide from 'src/component/NavBar/TopRightSide';
 
 const NavBar = () => {
   return (
-    <div className='relative h-[32px] w-full shadow-lg backdrop-blur-md'>
-      <div className='h-[32px] w-full bg-[#d2d2d2] opacity-20 drop-shadow'></div>
-      <div className='absolute top-0 flex h-full w-full justify-between px-5'>
-        <TopLeftSide />
-        <TopRightSide />
+    <div className='h-[32px] w-full shadow-lg backdrop-blur-md'>
+      <div className='h-[32px] w-full bg-[#d2d2d2] bg-opacity-20 drop-shadow'>
+        <div className='flex h-full w-full justify-between px-5'>
+          <TopLeftSide />
+          <TopRightSide />
+        </div>
       </div>
     </div>
   );
