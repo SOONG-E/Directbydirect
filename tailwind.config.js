1e4483ad9cdda4b88197746d4adb939c9ade882c
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-08 08:56:37 +0900

    chore: tailwindcss-animated 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -9,5 +10,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animated')],
 };
