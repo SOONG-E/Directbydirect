607074d53605963f8bf67f92d641ca032a67972c
soong-e <yujelee@student.42seoul.kr>
2024-01-08 00:53:12 +0900

    feat: 로고 이미지 누르면 새로고침되게 함

--- src/component/NavBar/TopLeftSide.jsx
+++ src/component/NavBar/TopLeftSide.jsx
@@ -1,9 +1,20 @@
 const TopLeftSide = () => {
+  const onClickImg = () => {
+    window.location.reload();
+  };
+
   return (
-    <div className='flex text-midblack font-bold text-sm items-center'>
-      <img src='MiniLogo.png' className='w-6 mr-4' />
-      <div>DirectByDirect</div>
-      <div className='ml-4'>Gits</div>
+    <div className='relative flex items-center text-sm font-bold text-midblack'>
+      <img
+        src='MiniLogo.png'
+        className='mr-4 w-6 cursor-pointer'
+        onClick={onClickImg}
+      />
+      <button className='top-bar-item'>DirectByDirect</button>
+      <button className='top-bar-item ml-4'>Helps</button>
+      <button className='top-bar-item ml-4' onClick={onClickedGits}>
+        Gits
+      </button>
     </div>
   );
 };
