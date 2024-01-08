9420ee4696aa3641941cda753da6757a50e93beb
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-08 19:10:21 +0900

    feat: 터미널에 스크롤 추가

--- src/component/Terminal/History.jsx
+++ src/component/Terminal/History.jsx
@@ -1,6 +1,6 @@
 export default function History({ history }) {
   return (
-    <div className='h-full'>
+    <div className='h-full overflow-y-auto'>
       {history.map((x, idx) => (
         <div className='ml-2 flex items-center' key={idx}>
           <div className='mr-2 text-xl font-bold text-green-400'>&gt;</div>
