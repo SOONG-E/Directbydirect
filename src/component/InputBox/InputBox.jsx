8128078a2d9059b7865d035a765371949f708b95
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-08 10:09:41 +0900

    feat: 입력 필드 유효성 검사

--- src/component/InputBox/InputBox.jsx
+++ src/component/InputBox/InputBox.jsx
@@ -1,13 +1,8 @@
-import { useRecoilState } from 'recoil';
 import InputBoxHeader from 'src/component/InputBox/InputBoxHeader';
+import InputField from 'src/component/InputBox/InputField';
 import OkButton from 'src/component/InputBox/OkButton';
-import { rootDirNameState } from 'src/state/rootDirName';
 
 const InputBox = () => {
-  const [rootDirName, setRootDirName] = useRecoilState(rootDirNameState);
-
-  const handleChange = (e) => setRootDirName(e.target.value);
-
   return (
     <div className='flex h-screen items-center justify-center'>
       <div className='fixed left-0 top-0 flex h-full w-full items-center justify-center bg-[#d9d9d9]/[50%]'>
@@ -17,12 +12,7 @@ const InputBox = () => {
             <p className='text-left text-[13px] font-semibold text-[#262626]'>
               루트 디렉터리의 이름을 입력해 주세요:
             </p>
-            <input
-              type='text'
-              value={rootDirName}
-              onChange={handleChange}
-              className='mt-[6px] h-[23px] w-full rounded-[5px] border bg-white p-2 text-black drop-shadow'
-            />
+            <InputField />
             <div className='mt-[11px] flex justify-end'>
               <OkButton text='Go' />
             </div>
---
+++ src/component/InputBox/InputField.jsx
@@ -0,0 +1,44 @@
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { rootDirNameState } from 'src/state/rootDirName';
+
+const InputField = () => {
+  const [rootDirName, setRootDirName] = useRecoilState(rootDirNameState);
+  const [isValid, setIsValid] = useState(true);
+  const [shake, setShake] = useState(false);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setRootDirName(value);
+    const regex = /^.{1,20}$/;
+    setIsValid(regex.test(value));
+  };
+
+  const inputClass = classNames(
+    'mt-[6px] h-[23px] w-full rounded-[5px] border bg-white p-2 text-black drop-shadow focus:outline-none',
+    {
+      'animate-shake animate-duration-[175ms]': shake,
+      'border-2 border-rose-500': !isValid,
+    }
+  );
+
+  useEffect(() => {
+    if (isValid) return;
+    setTimeout(() => {
+      setShake(true);
+      setTimeout(() => setShake(false), 820);
+    }, 200);
+  }, [isValid]);
+
+  return (
+    <input
+      type='text'
+      value={rootDirName}
+      onChange={handleChange}
+      className={inputClass}
+    />
+  );
+};
+
+export default InputField;
