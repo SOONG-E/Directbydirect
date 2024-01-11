23fa1d4c5922533c2f7773c89bbe4046a4bf0fab
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-11 14:34:49 +0900

    fix: 터미널 히스토리에 누락된 공백 추가

--- src/component/Terminal/History.jsx
+++ src/component/Terminal/History.jsx
@@ -10,7 +10,7 @@ export default function History() {
         <div className='ml-2 items-center' key={idx}>
           <div className='flex space-x-2'>
             <p className='text-xl font-bold text-green-400'>&gt;</p>
-            <p className='pt-0.5 font-semibold text-white'>{x}</p>
+            <p className='pt-0.5 font-semibold text-white'>{x.join(' ')}</p>
           </div>
           <div className='flex'>
             <p className='font-semibold text-white'>{history.error[idx]}</p>
