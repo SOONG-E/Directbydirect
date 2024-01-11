d77ca85ef82d9c4af47b1c60e4bdaf1f8e831a3f
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-11 15:03:57 +0900

    feat: 트리 뷰 구현

--- src/component/DirectoryTree/TreeView.jsx
+++ src/component/DirectoryTree/TreeView.jsx
@@ -1,8 +1,6 @@
 import File from 'src/component/DirectoryTree/File';
 
 const TreeView = ({ tree }) => {
-  const child = tree.getChild();
-
   return (
     <div className='flex-1 rounded-b-md bg-[#efefef] bg-opacity-90'>
       {tree.getParent() === null && (
@@ -10,12 +8,9 @@ const TreeView = ({ tree }) => {
           <File tree={tree} />
         </div>
       )}
-      {/* {tree.forEach((child) => (
-        <div>
-          <IconPick tree={tree} />
-          {child.child.size !== 0 && <Detail tree={child} />}
-        </div>
-      ))} */}
+      {tree.getChild().forEach((subtree) => (
+        <TreeView tree={subtree} />
+      ))}
     </div>
   );
 };
