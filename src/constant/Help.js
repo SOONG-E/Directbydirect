65cd246bc573562cdd81dda17e121e34f480c385
oio337a <oio337a@gmail.com>
2024-01-11 16:10:01 +0900

    feat: help 세부 설명과 예시 추가

--- src/constant/Help.js
+++ src/constant/Help.js
@@ -2,41 +2,54 @@ export const HELP = [
   {
     CMD: 'cd',
     TEXT: 'Change directory',
+    CONTENT: '현재 작업 디렉터리의 위치를 바꾸는 명령어입니다.👉',
+    EXAMPLE: 'cd ../home',
   },
   {
     CMD: 'clear',
     TEXT: 'Clear terminal',
+    CONTENT: '터미널을 깨끗하게 지웁니다.🧹',
+    EXAMPLE: 'clear',
   },
   {
     CMD: 'cp',
     TEXT: 'Copy file',
+    CONTENT: '파일을 복사하는 명령어입니다.📝',
+    EXAMPLE: 'cp file1 file2',
   },
   {
     CMD: 'help',
     TEXT: 'List all commands',
-  },
-  {
-    CMD: 'ls',
-    TEXT: 'List directory',
+    CONTENT: '모든 명령어를 나열합니다. 한번 쳐보세요. 🤗',
   },
   {
     CMD: 'mkdir',
     TEXT: 'Create directory',
+    CONTENT: '디렉터리를 생성합니다.📂',
+    EXAMPLE: 'mkdir directory',
   },
   {
     CMD: 'mv',
     TEXT: 'Move file',
+    CONTENT: '파일을 이동합니다.🚚',
+    EXAMPLE: 'mv file1 file2',
   },
   {
     CMD: 'pwd',
     TEXT: 'Print working directory',
+    CONTENT: '현재 작업 디렉터리의 위치를 출력합니다.📌',
+    EXAMPLE: 'pwd',
   },
   {
     CMD: 'rm',
     TEXT: 'Remove file',
+    CONTENT: '파일을 삭제합니다.🗑',
+    EXAMPLE: 'rm file',
   },
   {
     CMD: 'touch',
     TEXT: 'Create file',
+    CONTENT: '파일을 생성합니다.📄',
+    EXAMPLE: 'touch file',
   },
 ];
