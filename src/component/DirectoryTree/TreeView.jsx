b7d81e70d4d103ac3d5c6bef53f039470351cfa1
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-11 16:48:25 +0900

    fix: 트리 뷰 로직 수정

--- src/component/DirectoryTree/TreeView.jsx
+++ src/component/DirectoryTree/TreeView.jsx
@@ -1,16 +1,17 @@
 import File from 'src/component/DirectoryTree/File';
 
 const TreeView = ({ tree }) => {
+  const child = tree.getChild();
+
   return (
     <div className='flex-1 rounded-b-md bg-[#efefef] bg-opacity-90'>
-      {tree.getParent() === null && (
-        <div className='flex items-center gap-1'>
-          <File tree={tree} />
-        </div>
-      )}
-      {tree.getChild().forEach((subtree) => (
-        <TreeView tree={subtree} />
-      ))}
+      <div className='flex items-center gap-1'>
+        <File tree={tree} />
+      </div>
+      {child &&
+        [...child.values()].map((subtree, index) => (
+          <TreeView key={index} tree={subtree} />
+        ))}
     </div>
   );
 };
