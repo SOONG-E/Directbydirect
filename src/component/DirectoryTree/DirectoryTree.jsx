b6869355929a471936b7b5af68726d17144d0553
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-11 17:02:40 +0900

    fix: 트리 뷰가 리렌더링되지 않는 문제 수정

--- src/component/DirectoryTree/DirectoryTree.jsx
+++ src/component/DirectoryTree/DirectoryTree.jsx
@@ -1,15 +1,20 @@
+import { useEffect } from 'react';
 import Draggable from 'react-draggable';
 import { useRecoilValue } from 'recoil';
 import TreeView from 'src/component/DirectoryTree/TreeView';
 import TopBar from 'src/component/common/TopBar';
+import { historyState } from 'src/state/history';
 import { rootState } from 'src/state/root';
 import { rootDirNameState } from 'src/state/rootDirName';
 
 export default function DirectoryTree() {
   const root = useRecoilValue(rootState);
   const rootDirName = useRecoilValue(rootDirNameState);
+  const history = useRecoilValue(historyState);
 
   root.setName(rootDirName);
+
+  useEffect(() => {}, [history]);
 
   return (
     <Draggable defaultPosition={{ x: 400, y: 50 }} bounds='body'>
