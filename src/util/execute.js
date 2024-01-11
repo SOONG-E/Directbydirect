bf8a73cf84b7a87e5883c05b2642ef20323422e6
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-11 18:36:50 +0900

    feat: clear 명령어 기능 구현

--- src/util/execute.js
+++ src/util/execute.js
@@ -3,6 +3,7 @@ import Builtin from 'src/util/Builitin';
 const table = new Map();
 table.set('cd', Builtin.cd);
 table.set('chmod', Builtin.chmod);
+table.set('clear', Builtin.clear);
 table.set('cp', Builtin.cp);
 table.set('mkdir', Builtin.mkdir);
 table.set('mv', Builtin.mv);
@@ -10,14 +11,14 @@ table.set('pwd', Builtin.pwd);
 table.set('rm', Builtin.rm);
 table.set('touch', Builtin.touch);
 
-const execute = (splittedCmd, cwdObj) => {
+const execute = (splittedCmd, obj) => {
   const cmd = splittedCmd[0];
   const arg = splittedCmd.slice(1);
   const func = table.get(cmd);
   if (func === undefined) {
     return { output: [], error: [`${cmd}: command not found`] };
   }
-  return func(arg, cwdObj);
+  return func(arg, obj);
 };
 
 export default execute;
