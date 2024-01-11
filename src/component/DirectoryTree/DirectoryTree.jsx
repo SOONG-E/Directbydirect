428ff325d78845ae7addcc7f85a32b1dafd31c01
soong-e <yujelee@student.42seoul.kr>
2024-01-11 15:49:24 +0900

    style: 윈도우 크기 조정

--- src/component/DirectoryTree/DirectoryTree.jsx
+++ src/component/DirectoryTree/DirectoryTree.jsx
@@ -13,7 +13,7 @@ export default function DirectoryTree() {
 
   return (
     <Draggable defaultPosition={{ x: 400, y: 50 }} bounds='body'>
-      <div className='absolute flex h-5/6 w-3/5 min-w-60 flex-col'>
+      <div className='window-height tree-width absolute flex flex-col'>
         <TopBar />
         <TreeView tree={root} />
       </div>
--- src/component/Terminal/Terminal.jsx
+++ src/component/Terminal/Terminal.jsx
@@ -6,7 +6,7 @@ import TopBar from 'src/component/common/TopBar';
 export default function Terminal() {
   return (
     <Draggable defaultPosition={{ x: 20, y: 50 }} bounds='body'>
-      <div className='absolute flex h-5/6 min-h-96 w-[360px] flex-col justify-between rounded-md bg-black shadow-lg'>
+      <div className='window-height absolute flex w-[360px] flex-col justify-between rounded-md bg-black shadow-lg'>
         <TopBar title='Terminal' />
         <History />
         <Prompt />
