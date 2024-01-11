2e33390eb06d0f2f307b24fdadde1ff7622d5718
soong-e <yujelee@student.42seoul.kr>
2024-01-11 15:34:20 +0900

    feat: 사이트 설명 작성

--- src/component/NavBar/Introduction.jsx
+++ src/component/NavBar/Introduction.jsx
@@ -1,7 +1,12 @@
 const Introduction = () => {
+  const instruction =
+    '디렉토리 구조를 변경할 수 있는 명령어를 연습해볼 수 있는 웹사이트입니다.';
+  const githubLink = 'https://github.com/SOONG-E/Directbydirect';
+
   return (
     <div className='nav-bar-box left-12 text-left'>
-      <div>설명설명</div>
+      <div>{instruction}</div>
+      <a href={githubLink}>깃허브 바로가기</a>
       <div className='my-2 h-[0px] w-full border-t border-gray-400 px-2'></div>
       <div className='text-xs'>{'버전 정보: v1.0'}</div>
     </div>
