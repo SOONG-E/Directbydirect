28c5781c802452b8158c8f6304665a3a3ec17ee5
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-17 11:33:00 +0900

    style: 디렉터리 트리 내부 간격 조정

--- src/component/DirectoryTree/TreeView.jsx
+++ src/component/DirectoryTree/TreeView.jsx
@@ -4,7 +4,7 @@ const TreeView = ({ tree }) => {
   const child = tree.getChild();
 
   return (
-    <div className='flex-1 rounded-b-md bg-[#efefef] bg-opacity-90'>
+    <div className='flex-1 space-y-1 rounded-b-md bg-[#efefef] bg-opacity-90'>
       <File tree={tree} />
       {child &&
         [...child.values()].map((subtree, index) => (
