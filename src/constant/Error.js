673199f3a22aa0e71406dd055e261ab86ca2ef0d
jjongs2 <jjongs@hanyang.ac.kr>
2024-01-22 16:14:34 +0900

    refactor: 에러 메시지 상수화

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -3,6 +3,7 @@ export const ERROR = Object.freeze({
   EIDENTICAL: 'are identical (not copied).',
   EISDIR: 'Is a directory',
   EMARG: 'Too many arguments',
+  ENOCMD: 'command not found',
   ENOENT: 'No such file or directory',
   ENOTDIR: 'Not a directory',
   EUSAGE: "Wrong usage\nTry 'help' for more information.",
