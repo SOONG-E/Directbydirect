e2969d0d43edcf364fe4d0091fd31d770792b003
oio337a <oio337a@gmail.com>
2024-01-22 15:35:40 +0900

    feat: 설명 항목 추가

--- src/constant/Help.js
+++ src/constant/Help.js
@@ -5,6 +5,8 @@ export const HELP = [
     CONTENT: '현재 작업 디렉터리의 위치를 바꾸는 명령어입니다.',
     EXAMPLE: 'cd ../home',
     ICON: '🚪',
+    EXPLAINMENT:
+      '상위의 home 으로 이동합니다.\n../ => 상위 디렉토리 이동\n./ => 현재 디렉토리 이동\n',
   },
   {
     CMD: 'clear',
@@ -19,11 +21,13 @@ export const HELP = [
     CONTENT: '파일을 복사하는 명령어입니다.',
     EXAMPLE: 'cp file1 file2',
     ICON: '📝',
+    EXPLAINMENT:
+      'file1을 file2로 복사합니다.\n👉 cp ../file1 ../file2 는 무엇일까요?',
   },
   {
     CMD: 'help',
     TEXT: 'List all commands',
-    CONTENT: '모든 명령어를 나열합니다. 한번 쳐보세요.',
+    CONTENT: '도움이 필요하면 한번 쳐보세요. 이곳에서 기다릴게요 :)',
     EXAMPLE: 'help',
     ICON: '🤗',
   },
@@ -33,6 +37,8 @@ export const HELP = [
     CONTENT: '디렉터리를 생성합니다.',
     EXAMPLE: 'mkdir directory',
     ICON: '📂',
+    EXPLAINMENT:
+      'mkdir directory1 directory2\n => 공백으로 여러개의 디렉터리를 생성할 수 있습니다.\n👉 mkdir ../dir 은 무엇일까요??',
   },
   {
     CMD: 'mv',
@@ -40,6 +46,8 @@ export const HELP = [
     CONTENT: '파일을 이동합니다.',
     EXAMPLE: 'mv file1 file2',
     ICON: '🚚',
+    EXPLAINMENT:
+      'file1을 file2로 이동합니다.\n👉 mv ../file1 ../file2 는 무엇일까요?',
   },
   {
     CMD: 'pwd',
@@ -47,6 +55,7 @@ export const HELP = [
     CONTENT: '현재 작업 디렉터리의 위치를 출력합니다.',
     EXAMPLE: 'pwd',
     ICON: '📌',
+    EXPLAINMENT: '여러분은 지금 어디에 있나요?',
   },
   {
     CMD: 'rm',
@@ -54,6 +63,8 @@ export const HELP = [
     CONTENT: '파일을 삭제합니다.',
     EXAMPLE: 'rm file',
     ICON: '🗑',
+    EXPLAINMENT:
+      'rm file1 file2\n => 공백으로 여러개의 파일을 삭제할 수 있습니다.\n👉 rm -rf directory 는 무엇일까요?',
   },
   {
     CMD: 'touch',
@@ -61,5 +72,7 @@ export const HELP = [
     CONTENT: '파일을 생성합니다.',
     EXAMPLE: 'touch file',
     ICON: '📄',
+    EXPLAINMENT:
+      'touch file1 file2\n => 공백으로 여러개의 파일을 생성할 수 있습니다.\n👉 touch ../file 은 무엇일까요?',
   },
 ];
