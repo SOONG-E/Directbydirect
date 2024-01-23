f1e675f8d67a22e3e27c4f2141985bb424a56a4a
oio337a <oio337a@gmail.com>
2024-01-23 17:14:27 +0900

    style: terminal 글자 가운데 정렬, 동그라미 크기 줄임

--- src/component/common/TopBar.jsx
+++ src/component/common/TopBar.jsx
@@ -2,11 +2,13 @@ export default function TopBar({ title }) {
   return (
     <div className='flex justify-between rounded-t-md border border-gray-400 bg-bar-gray shadow-sm'>
       <div className='ml-2 flex items-center justify-center space-x-1'>
-        <div className='aspect-square h-4 w-4 rounded-full bg-[#ff2b2b] shadow-md' />
-        <div className='aspect-square h-4 w-4 rounded-full bg-[#ffc700] shadow-md' />
-        <div className='aspect-square h-4 w-4 rounded-full bg-[#04b300] shadow-md' />
+        <div className='aspect-square h-3 w-3 rounded-full bg-[#ff2b2b] shadow-md' />
+        <div className='aspect-square h-3 w-3 rounded-full bg-[#ffc700] shadow-md' />
+        <div className='aspect-square h-3 w-3 rounded-full bg-[#04b300] shadow-md' />
       </div>
-      <p className='h-5 flex-1 font-semibold shadow-sm'>{title}</p>
+      <p className='mr-8 flex h-5 flex-1 items-center justify-center font-semibold shadow-sm'>
+        {title}
+      </p>
     </div>
   );
 }
